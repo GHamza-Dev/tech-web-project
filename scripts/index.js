4ad0d516590d7d01169be9579e67e33bb46de258
Ghamza-Dev <elgassaihamzam54@gmail.com>
2021-01-13 16:38:16 +0100

    Filter Added

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,20 @@
 
 const menuBtn = document.querySelector('.btn-menu');
 const nav_menu = document.querySelector('.container header nav');
-
-
+const filterIcon = document.querySelector('.filter_icon');
+const filterForm = document.querySelector('.filter')
 
 
 // Functions 
+
+/**** Show hide menu */
 menuBtn.addEventListener('click',()=>{
     menuBtn.classList.toggle('clicked-mn-btn');
     nav_menu.classList.toggle('show_menu');
+});
+
+
+/**** Show hide filter form */
+filterIcon.addEventListener('click',()=>{
+    filterForm.classList.toggle("show_filter_form");
 });
